15b91f405369812cd53e3048d8f93114a1d38868
mrrahulkaran <karanr9991@gmail.com>
2023-07-05 19:55:42 +0530

    my second sum

--- sum/addition.java
+++ sum/addition.java
@@ -5,7 +5,7 @@ public class addition {
 	
 	public static void main(String[] args) {
 		
-		int n1 = 225, n2 = 115, sum;  
+		int n1 = 725, n2 = 815, sum;  
 		sum = n1 + n2;  
 		System.out.println("The sum of numbers is: "+sum);  
 		
